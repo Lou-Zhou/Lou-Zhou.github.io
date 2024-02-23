51dcd03286759a42d8f4300396efcba21028bf2d
Lou Zhou <86981747+Lou-Zhou@users.noreply.github.com>
2024-02-22 23:25:41 -0600

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -33,7 +33,7 @@ const CONFIG = {
       // To hide the `External Projects` section, keep it empty.
       projects: [
         {
-          title: 'Analyzing Travel Fatigue and Performancea',
+          title: 'Analyzing Travel Fatigue and Performance',
           description:
             'Sport Analytics Project looking into whether travel and fatigue had any real effect on team performance in Baseball. Submitted for the 36-hour 2023 Rice Datathon, finishing 2nd overall out of 59 teams.',
           imageUrl:
